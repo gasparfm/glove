2d1564e85ad69d029e449086e26bcd08235b0516
Gaspar Fernandez <gaspar.fernandez@totaki.com>
2015-04-04 21:33:22 +0200

    Backup purposes. Builds but lacks some features

--- glovehttpserver.cpp
+++ glovehttpserver.cpp
@@ -1,3 +1,47 @@
+/**
+*************************************************************
+* @file glovehttpserver.cpp
+* @brief Tiny http C++11 server using glove with routing and
+*        come cool stuff
+*
+* @author Gaspar Fernández <blakeyed@totaki.com>
+* @version
+* @date 03 apr 2015
+*
+* Notes:
+*  - Based on ideas taken on some PHP Frameworks, Java Servlets and more
+*
+* Changelog:
+*  20150403 : Begin this project
+*  20140404 : Added HTTP Response data
+*
+* To-do:
+*   - Loooots of things
+*   - Be able to process FastCGI requests to use with Apache/NGinx and more
+*
+* MIT Licensed:
+* Copyright (c) 2014 Gaspar Fernández
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+* 
+*************************************************************/
+
 #include "glovehttpserver.h"
 #include <chrono>
 #include <thread>
